2686cd0e278b9f29e2ae24366f65fce002395a73
Kayleigh Bell <kayleightuppen@gmail.com>
2021-05-10 16:53:42 +0100

    used spread and sort
    Used spread and sort to merge the arrays together and sort in order from lowest to highest

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,8 +1,8 @@
-describe('flatten array', function () {
-  it('should flatten an array', function () {
-    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
+const arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]];
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
-    expect(arr).toEqual(expected);
-  });
-});
+        const merged = [].concat(...arr);
+        const sorted = merged.sort((a, b)=>a-b);
+
+
+        console.log(sorted);  
